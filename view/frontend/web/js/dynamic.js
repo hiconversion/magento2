08c1290228778a9f11e1122675c1b4fc510de01c
Mark Conatser <mconatser@hiconversion.com>
2018-06-26 18:55:57 -0400

    fixed validation issues

--- view/frontend/web/js/dynamic.js
+++ view/frontend/web/js/dynamic.js
@@ -1,3 +1,20 @@
+/**
+ * HiConversion
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * [http://opensource.org/licenses/MIT]
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @Copyright © 2015 HiConversion, Inc. All rights reserved.
+ * @license [http://opensource.org/licenses/MIT] MIT License
+ */
+
 define([
     'jquery',
     'Magento_Customer/js/customer-data',
@@ -25,7 +42,8 @@ define([
     if ($.isEmptyObject(user) || $.isEmptyObject(cart)) {
         try {
             customerData.reload(['hic-user-data', 'hic-cart-data']);
-        } catch (ex) {}
+        } catch (ex) {
+        }
     }
     
     if (isEnabled(user)) {
@@ -35,14 +53,13 @@ define([
         writeWindowObject('user', user);
         writeWindowObject('cart', cart);
     
-    
         hicUserData.subscribe(function (user) {
             writeWindowObject('user', user);
         });
-    
     
         hicCartData.subscribe(function (cart) {
             writeWindowObject('cart', cart);
         });
     }
     });
+    
